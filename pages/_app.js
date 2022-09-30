1ef723409ef6a200d88fb132a8187f4807eb3f52
kharioki <tony.kharioki@gmail.com>
2022-09-30 23:57:20 +0300

    🎨 components and pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { Fragment } from "react";
+import Layout from "../components/layout/Layout";
+import { NotificationContextProvider } from "../context/notificationContext";
+import { UserContextProvider } from "../context/userContext";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <NotificationContextProvider>
+        <UserContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </UserContextProvider>
+      </NotificationContextProvider>
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
